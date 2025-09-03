8bacb9e131a0f91f1754ccfb4b7012bda5cebce3
LeanderStern <rednael3@freenet.de>
2025-09-03 14:59:23 +0200

    updated splash screen logic and added session storage, changed project branding to 'Stern Screenplays' with theme and favicon adjustments

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,17 +1,30 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay, } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
+import { Preset } from '@primeuix/themes/types';
+
+const sternSPConfig: Preset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{amber.50}',
+            100: '{amber.100}',
+            200: '{amber.200}',
+            300: '{amber.300}',
+            400: '{amber.400}',
+            500: '{amber.500}',
+            600: '{amber.600}',
+            700: '{amber.700}',
+            800: '{amber.800}',
+            900: '{amber.900}',
+            950: '{amber.950}'
+        }
+    }
+})
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,9 +34,10 @@ export const appConfig: ApplicationConfig = {
       provideRouter(appRoutes),
       provideAnimationsAsync(),
       providePrimeNG({
+          ripple: true,
           theme: {
-              preset: Aura
-          }
+              preset: sternSPConfig
+          },
       })
   ],
 };
--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Button } from 'primeng/button';
+import { ALREADY_VISITED } from '../shared/constants/session-storage';
 
 @Component({
-    imports: [RouterModule, Button],
+    imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'Frontend';
+  public showSplashScreen: boolean
+
+  public constructor() {
+  }
+
+  public ngOnInit(): void {
+      if (typeof window === 'undefined' || !window.sessionStorage) {
+          return
+      }
+      this.showSplashScreen = Boolean(sessionStorage.getItem(ALREADY_VISITED));
+      if (this.showSplashScreen === false) {
+          sessionStorage.setItem(ALREADY_VISITED, 'true');
+      }
+  }
 }
